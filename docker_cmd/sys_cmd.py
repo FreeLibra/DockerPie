25966ce52bf2a7057cbbab15fbd387df5165bade
FreeLibra <windelf731@gmail.com>
2017-12-23 21:09:32 +0800

    Modify the log file and directory name

--- docker_cmd/sys_cmd.py
+++ docker_cmd/sys_cmd.py
@@ -5,8 +5,9 @@ def list_docker_images():
     result = subprocess.getstatusoutput('docker images')
     print(result[0])
     print(result[1])
-    with open('./output/output.txt','w') as fp:
+    with open('./log/temp.txt', 'w') as fp:
         fp.write(result[1])
+
 
 if __name__ == '__main__':
     list_docker_images()
