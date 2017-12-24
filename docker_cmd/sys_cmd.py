864ebc9b57d4eaa04063dda7aed45e09d4bc5643
FreeLibra <windelf731@gmail.com>
2017-12-24 16:27:21 +0800

    solve re

--- docker_cmd/sys_cmd.py
+++ docker_cmd/sys_cmd.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import subprocess
+import numpy as np
 
 LOG_DIR = './log/temp.txt'
 
@@ -11,6 +12,15 @@ def list_docker_images(log_dir=LOG_DIR):
     # print(result[1])
     with open(log_dir, 'w') as fp:
         fp.write(result[1])
+    line_list = result[1].split('\n')
+    print(line_list[0])
+    word = line_list[0]
+    print(word)
+    print(len(word))
+    print(type(word))
+    # print('------------')
+    # for ele in word:
+    #     print(ele)
     return result
 
 
