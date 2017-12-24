feb3b9634d42125f6ed33bec619c76a4fa182499
FreeLibra <windelf731@gmail.com>
2017-12-24 18:11:58 +0800

    re.sub("\s{2,}", "\t", line).split("\t")

--- docker_cmd/sys_cmd.py
+++ docker_cmd/sys_cmd.py
@@ -2,26 +2,39 @@
 # -*- coding:utf-8 -*-
 import subprocess
 import numpy as np
+import pandas as pd
+import re
 
-LOG_DIR = './log/temp.txt'
+LOG_FILE = './log/temp.txt'
 
 
-def list_docker_images(log_dir=LOG_DIR):
-    result = subprocess.getstatusoutput('docker images')
-    # print(result[0])
-    # print(result[1])
-    with open(log_dir, 'w') as fp:
-        fp.write(result[1])
-    line_list = result[1].split('\n')
-    print(line_list[0])
-    word = line_list[0]
-    print(word)
-    print(len(word))
-    print(type(word))
-    # print('------------')
-    # for ele in word:
-    #     print(ele)
-    return result
+def result_handler(raw_output, log_file=LOG_FILE):
+    """
+    Handle the raw_output words in lines
+    word = re.sub("[^\w]", " ", line).split()
+    word = re.sub("\s", " ", line).split()
+    :param raw_output: raw_output words in lines from the terminal
+    :param log_dir: the output temp file
+    :return:
+    """
+    with open(log_file, 'w') as fp:
+        fp.write(raw_output[1])
+    result_item_list = []
+    line_list = raw_output[1].split('\n')
+    print(line_list)
+    for line in line_list:
+        result_item_list.append(re.sub("\s{2,}", "\t", line).split("\t"))
+    print(result_item_list)
+    return raw_output[0], result_item_list
+
+
+def list_docker_images(log_file=LOG_FILE):
+    """
+    list the docker images
+    :return: status,the items in docker images  list
+    """
+    raw_result = subprocess.getstatusoutput('docker images')
+    return result_handler(raw_result, log_file=LOG_FILE)
 
 
 if __name__ == '__main__':
